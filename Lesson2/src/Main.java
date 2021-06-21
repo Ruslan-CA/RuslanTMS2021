3152207e23e0666fc7b0d8c9d4e1dc91ca2783f3
Ruslan-CA <86113162+Ruslan-CA@users.noreply.github.com>
2021-06-21 22:26:28 +0300

    test git

--- Lesson2/src/Main.java
+++ Lesson2/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
+        String text = "Привет а \nв разных строках";
+        System.out.println(text);
+
+        System.out.println(Constants.LIMIT);
     }
 }
